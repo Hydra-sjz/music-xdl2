75b8ecaa0813f13412535edc3e9ef1f08487f514
Hydra-sjz <119828208+Hydra-sjz@users.noreply.github.com>
2022-12-05 08:24:09 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/hype-rolex/music-xdl.git /music-xdl
+  git clone https://github.com/Hydra-sjz/music-xdl.git /music-xdl
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /music-xdl
