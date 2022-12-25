eebe750d95066ece4f8c89542a3a683a234b5329
Hydra-sjz <119828208+Hydra-sjz@users.noreply.github.com>
2022-12-25 19:13:40 +0530

    Update Song_dl.py

--- plugins/Song_dl.py
+++ plugins/Song_dl.py
@@ -44,7 +44,7 @@ def time_to_seconds(time):
 
 @Client.on_message(filters.private & filters.command("start"))
 async def start_command(_, message):
-    await message.reply_text("hi\nim music download bot for my group ‼️\nyou can download songs from my group just type /s or /song then write your song name.\ne.g: `/s alone`")
+    await message.reply_text(f"Hi {message.from_user.mention}\n\nIm am music download bot for @songdownload_group group ‼️\nyou can download songs from there, just type /s or /song then write your song name.\ne.g: `/s alone`\n\nhttps://t.me/songdownload_group")
 
 @Client.on_message(filters.group & filters.command(["song", "song@Musicx_dlbot", "s"]))
 def song(_, message):
