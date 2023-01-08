c4032bddb00af17a90c5e82fba991e2510ef4843
Hydra-sjz <119828208+Hydra-sjz@users.noreply.github.com>
2023-01-08 23:14:43 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,18 +1,37 @@
-from pyrogram import Client
-import os
-from config import BOT_TOKEN, API_ID, API_HASH
+import logging
+import logging.config
+from pyrogram import Client 
+from config import API_ID, API_HASH, BOT_TOKEN
+
+logging.config.fileConfig('logging.conf')
+logging.getLogger().setLevel(logging.INFO)
+logging.getLogger("pyrogram").setLevel(logging.ERROR)
 
 
+class Bot(Client):
 
+    def __init__(self):
+        super().__init__(
+            name="song_dl",
+            api_id=API_ID,
+            api_hash=API_HASH,
+            bot_token=BOT_TOKEN,
+            workers=50,
+            plugins={"root": "plugins"},
+            sleep_threshold=5,
+        )
 
-    app = Client(
-        "music_dl",
-        bot_token=BOT_TOKEN,
-        api_id=API_ID,
-        api_hash=API_HASH,
-        plugins=plugins,
-        workers=300,
-    )
+    async def start(self):
+       await super().start()
+       me = await self.get_me()
+       self.mention = me.mention
+       self.username = me.username       
+       logging.info(f"{me.first_name} 𝚂𝚃𝙰𝚁𝚃𝙴𝙳 ⚡️⚡️⚡️")
+      
 
-
-app.run()
+    async def stop(self, *args):
+      await super().stop()      
+      logging.info("Bot Stopped")
+        
+bot = Bot()
+bot.run()
