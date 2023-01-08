93672f010768b45ef9615734e07cf917494f6645
Hydra-sjz <119828208+Hydra-sjz@users.noreply.github.com>
2023-01-08 12:03:57 +0530

    Update and rename start to start.sh

---
+++ start.sh
@@ -0,0 +1,12 @@
+if [ -z $UPSTREAM_REPO ]
+then
+  echo "Cloning main Repository"
+  git clone https://github.com/Hydra-sjz/music-xdl.git /music-xdl
+else
+  echo "Cloning Custom Repo from $UPSTREAM_REPO "
+  git clone $UPSTREAM_REPO /music-xdl
+fi
+cd /Midukki-RoBoT
+pip3 install -U -r requirements.txt
+echo "Starting music-xdl-RoBot...."
+python3 -m music-xdl
