721a88c2f616b40eb4627c53f91b9bab649747da
Hydra-sjz <119828208+Hydra-sjz@users.noreply.github.com>
2023-01-08 15:12:51 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /music-xdl
 fi
-cd /Midukki-RoBoT
+cd /music-xdl
 pip3 install -U -r requirements.txt
 echo "Starting music-xdl-RoBot...."
 python3 -m bot.py
