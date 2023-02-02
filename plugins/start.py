94b6df8d140e6616f954a760d9065ec3ee4fde3a
Hydra-sjz <119828208+Hydra-sjz@users.noreply.github.com>
2023-02-02 22:25:05 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -6,4 +6,4 @@ from pyrogram import Client, filters
 
 @Client.on_message(filters.private & filters.command("start"))
 async def start_command(_, message):
-    await message.reply_text(f"Hi {message.from_user.mention}\n\nIm am music download bot for @songdownload_group group ‼️\nyou can download songs from there, just type /s or /song then write your song name.\ne.g: `/s alone`\n\nhttps://t.me/songdownload_group")
+    await message.reply_text(f"»━━━━━━«[𝗠ᴜsɪᴄ✘Dʟ]»━━━━━━«\n\n♦️Hi there {message.from_user.mention} ♥️♣️\n\nMy Name is 🎶[𝗠ᴜsɪᴄ✘Dʟ](t.me/Musicx_dlbot) Bot,and I'm a simple and 🏖️Advanced Audio & Video downloader bot with higher quality 🎛️.\njust type /s or /song then write your song name.\ne.g: `/s alone`\n\n»━━━━━━━━━━━━━━━━━━«")
