019b804de2805fe37b9e94b27d1b73f3f7411a15
Hydra-sjz <119828208+Hydra-sjz@users.noreply.github.com>
2023-02-02 23:33:11 +0530

    Update user_info.py

--- plugins/user_info.py
+++ plugins/user_info.py
@@ -10,7 +10,7 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 @Client.on_message(filters.command("id"))
 async def id_(bot: Client, msg: Message):
 	if not msg.chat.type == "private":
-		main = f"**This {msg.chat.type} ɪᴅ is** `{msg.chat.id}`\n**User Id is:** `{msg.from_user.id}`"
+		main = f"**This {msg.chat.type} id is** `{msg.chat.id}`\n**User Id is:** `{msg.from_user.id}`"
 		if msg.reply_to_message:
 			if msg.reply_to_message.from_user:
 				main = f"{msg.reply_to_message.from_user.first_name} your id is `{msg.reply_to_message.from_user.id}`\n\n**Message_id:**\nhttps://t.me/{msg.chat.username}/{msg.reply_to_message.message_id}"
@@ -20,7 +20,7 @@ async def id_(bot: Client, msg: Message):
 		await msg.reply(main)
 	else:
 		if len(msg.command) == 1:
-			await msg.reply(f"Yᴏᴜʀ ᴛᴇʟᴇɢʀᴀᴍ ɪᴅ ɪs: `{msg.from_user.id}`", quote=True)
+			await msg.reply(f"Your telegram id is: `{msg.from_user.id}`", quote=True)
 		if len(msg.command) == 2:
 			try:
 				uname = msg.command[1]
@@ -39,7 +39,7 @@ async def id_(bot: Client, msg: Message):
 					user = await bot.get_chat(uname)
 					name = '@'+user.username if user.username else user.title
 				id = user.id
-				await msg.reply(f"{name}'s ɪᴅ ɪs `{id}`", quote=True)
+				await msg.reply(f"{name}'s id is `{id}`", quote=True)
 			except UsernameInvalid:
 				await msg.reply("Invalid Username.", quote=True)
 			except UsernameNotOccupied:
@@ -49,7 +49,7 @@ async def id_(bot: Client, msg: Message):
 @Client.on_message(filters.command(["stickerid"]))
 async def stickerid(bot, message):   
     if message.reply_to_message.sticker:
-       await message.reply(f"**ʜᴇʀᴇ ɪs ʏᴏᴜʀ sᴛɪᴄᴋᴇʀ ɪᴅ**  \n `{message.reply_to_message.sticker.file_id}` \n \n ** ᴜɴɪǫᴜᴇ ɪᴅ ɪs ** \n\n`{message.reply_to_message.sticker.file_unique_id}`", quote=True)
+       await message.reply(f"**Here is your sticker id**  \n `{message.reply_to_message.sticker.file_id}` \n \n ** Unique id is: ** \n\n`{message.reply_to_message.sticker.file_unique_id}`", quote=True)
     else: 
        await message.reply("ɴɪᴄᴇ,ɪᴛs ɴᴏᴛ ᴀ sᴛɪᴄᴋᴇʀ")
 
@@ -63,4 +63,4 @@ async def dc(bot, update):
         quote=True
     )
 
-START_TEXT = "Yᴏᴜʀ ᴛᴇʟᴇɢʀᴀᴍ ᴅᴄ ɪs : `{}`"
+START_TEXT = "Your dc is : `{}`"
