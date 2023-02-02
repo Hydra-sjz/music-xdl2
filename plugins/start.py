5b3c57b8745298fd5c3f62fb1dced3d66126340f
Hydra-sjz <119828208+Hydra-sjz@users.noreply.github.com>
2023-02-02 23:55:11 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -6,4 +6,4 @@ from pyrogram import Client, filters
 
 @Client.on_message(filters.private & filters.command("start"))
 async def start_command(_, message):
-    await message.reply_text(f"»━━━━━━«[𝗠ᴜsɪᴄ✘Dʟ]»━━━━━━«\n\n♦️Hi there {message.from_user.mention} ♥️♣️\n\nMy Name is 🎶[𝗠ᴜsɪᴄ✘Dʟ](t.me/Musicx_dlbot) Bot,and I'm a simple and 🏖️Advanced Audio & Video downloader bot with higher quality 🎛️.\njust type /s or /song then write your song name.\ne.g: `/s alone`\n\n»━━━━━━━━━━━━━━━━━━«")
+    await message.reply_photo(photo="https://telegra.ph/file/2b28898bba3f9644e5faf.jpg", caption=f"»━━━━━━«[𝗠ᴜsɪᴄ✘Dʟ]»━━━━━━«\n\n♦️Hi there {message.from_user.mention} ♥️♣️\n\nMy Name is 🎶[𝗠ᴜsɪᴄ✘Dʟ](t.me/Musicx_dlbot) Bot,and I'm a simple and 🏖️Advanced Audio & Video downloader bot with higher quality 🎛️.\njust type /s or /song then write your song name.\ne.g: `/s alone`\n\n»━━━━━━━━━━━━━━━━━━«")
