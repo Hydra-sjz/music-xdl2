6a8ec665cdefa9c5d341bbd99c05d8e3cc460387
Hydra-sjz <119828208+Hydra-sjz@users.noreply.github.com>
2023-02-05 15:29:27 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -1,5 +1,5 @@
 from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+#from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 
 
@@ -7,9 +7,5 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 @Client.on_message(filters.private & filters.command("start"))
 async def start_command(_, message):
-    await message.reply_photo(photo="https://telegra.ph/file/ceeca2da01f5d39550111.jpg", caption=f"»━━━━━━«[𝗠ᴜsɪᴄ✘Dʟ]»━━━━━━«\n\nHi there {message.from_user.mention} ♥️♣️\n\nMy Name is 🎶[𝗠ᴜsɪᴄ✘Dʟ](t.me/Musicx_dlbot) Bot,and I'm a simple and 🏖️Advanced Audio & Video downloader bot with higher quality 🎛️.\n\nHow it's work🤔\n <b>Audio 💽</b>\ntype /s or /song then write your song name.\ne.g: `/s alone`\n<b>Video 📹</b>\ntype /v or /video then write your song name.\ne.g: `/v hope`\n\n»━━━━━━━━━━━━━━━━━━«")
-                  reply_markup=InlineKeyboardMarkup([[
-                    InlineKeyboardButton("My Group", url="https://t.me/songdownload_group")
-                    ]]
-                    )
-                 )
+    await message.reply_photo(photo="https://telegra.ph/file/ceeca2da01f5d39550111.jpg", caption=f"»━━━━━━«[𝗠ᴜsɪᴄ✘Dʟ]»━━━━━━«\n\nHi there {message.from_user.mention} ♥️♣️\n\nMy Name is 🎶[𝗠ᴜsɪᴄ✘Dʟ](t.me/Musicx_dlbot) Bot,and I'm a simple and 🏖️Advanced Audio & Video downloader bot with higher quality 🎛️.\n\nHow it's work🤔\n <b>Audio 💽</b>\ntype /s or /song then write your song name.\ne.g: `/s alone`\n<b>Video 📹</b>\ntype /v or /video then write your song name.\ne.g: `/v hope`\nⓂ️©️ @songdownload_group\n\n»━━━━━━━━━━━━━━━━━━«")
+                  Y
