b1a60d4516971bad3e80be04349bea8621a4c1e7
Hydra-sjz <119828208+Hydra-sjz@users.noreply.github.com>
2023-02-10 00:51:37 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,30 +1,33 @@
-from pyrogram import Client 
-from config import API_ID, API_HASH, BOT_TOKEN, PORT
-
+from pyrogram import Client
+from config import *
+from route import web_server
 from aiohttp import web
-from plugins import web_server
-
 
 class Bot(Client):
-
     def __init__(self):
         super().__init__(
-            name="song_dl",
+            name="musicXdl",
             api_id=API_ID,
             api_hash=API_HASH,
             bot_token=BOT_TOKEN,
-            workers=50,
-            plugins={"root": "plugins"},
-            sleep_threshold=5,
+            workers=100,
+            plugins={"root": "main"},
+            sleep_threshold=10,
         )
-
-        #web-response
+    async def start(self):
+        await super().start()
+        me = await self.get_me()   
         app = web.AppRunner(await web_server())
         await app.setup()
         bind_address = "0.0.0.0"
-        await web.TCPSite(app, bind_address, PORT).start()
-    
-        
+        port = "8080"
+        await web.TCPSite(app, bind_address, port).start()   
+        print(f"{me.first_name} | @{me.username} 𝚂𝚃𝙰𝚁𝚃𝙴𝙳...⚡️")
+       
+    async def stop(self, *args):
+       await super().stop()      
+       print("Bot Restarting........")
+
 
 bot = Bot()
-bot.run()
+bot.run() 
